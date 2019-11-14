96eddb518298609130043fce673acb8b92904ea1
zhangxishuo <zhangxishuo1998@gmail.com>
2019-11-14 10:37:21 +0800

    init-tool

---
+++ tool/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Gateway_Label = new System.Windows.Forms.Label();
+            this.EDIT = new System.Windows.Forms.Button();
+            this.Gateway = new System.Windows.Forms.Label();
+            this.PoweredBy = new System.Windows.Forms.Label();
+            this.yunzhi = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // Gateway_Label
+            // 
+            this.Gateway_Label.AutoSize = true;
+            this.Gateway_Label.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Gateway_Label.Location = new System.Drawing.Point(6, 42);
+            this.Gateway_Label.Name = "Gateway_Label";
+            this.Gateway_Label.Size = new System.Drawing.Size(180, 33);
+            this.Gateway_Label.TabIndex = 0;
+            this.Gateway_Label.Text = "当前网关：";
+            // 
+            // EDIT
+            // 
+            this.EDIT.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.EDIT.Location = new System.Drawing.Point(12, 154);
+            this.EDIT.Name = "EDIT";
+            this.EDIT.Size = new System.Drawing.Size(363, 72);
+            this.EDIT.TabIndex = 2;
+            this.EDIT.Text = "一键修改网关";
+            this.EDIT.UseVisualStyleBackColor = true;
+            this.EDIT.Click += new System.EventHandler(this.EDIT_Click);
+            // 
+            // Gateway
+            // 
+            this.Gateway.AutoSize = true;
+            this.Gateway.Font = new System.Drawing.Font("宋体", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Gateway.Location = new System.Drawing.Point(160, 42);
+            this.Gateway.Name = "Gateway";
+            this.Gateway.Size = new System.Drawing.Size(0, 33);
+            this.Gateway.TabIndex = 3;
+            // 
+            // PoweredBy
+            // 
+            this.PoweredBy.AutoSize = true;
+            this.PoweredBy.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.PoweredBy.Location = new System.Drawing.Point(8, 282);
+            this.PoweredBy.Name = "PoweredBy";
+            this.PoweredBy.Size = new System.Drawing.Size(120, 21);
+            this.PoweredBy.TabIndex = 4;
+            this.PoweredBy.Text = "技术支持：";
+            // 
+            // yunzhi
+            // 
+            this.yunzhi.AutoSize = true;
+            this.yunzhi.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.yunzhi.Location = new System.Drawing.Point(149, 282);
+            this.yunzhi.Name = "yunzhi";
+            this.yunzhi.Size = new System.Drawing.Size(226, 21);
+            this.yunzhi.TabIndex = 5;
+            this.yunzhi.TabStop = true;
+            this.yunzhi.Text = "www.mengyunzhi.com";
+            this.yunzhi.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Yunzhi_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 331);
+            this.Controls.Add(this.yunzhi);
+            this.Controls.Add(this.PoweredBy);
+            this.Controls.Add(this.Gateway);
+            this.Controls.Add(this.EDIT);
+            this.Controls.Add(this.Gateway_Label);
+            this.Name = "Form1";
+            this.Text = "网关小助手(请以管理员身份运行)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Gateway_Label;
+        private System.Windows.Forms.Button EDIT;
+        private System.Windows.Forms.Label Gateway;
+        private System.Windows.Forms.Label PoweredBy;
+        private System.Windows.Forms.LinkLabel yunzhi;
+    }
+}
+
---
+++ tool/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace tool
+{
+    public partial class Form1 : Form
+    {
+        private IPAddress address;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try
+            {
+                this.address = GetDefaultGateway();
+                this.Gateway.Text = address.ToString();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("获取网关发生错误，请检查静态路由配置是否正确. " + exception.Message);
+            }
+        }
+
+        /**
+         * 获取默认网关
+         * https://stackoverflow.com/questions/13634868/get-the-default-gateway
+         */
+        private IPAddress GetDefaultGateway()
+        {
+            return NetworkInterface
+                .GetAllNetworkInterfaces()
+                .Where(n => n.OperationalStatus == OperationalStatus.Up)
+                .Where(n => n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .SelectMany(n => n.GetIPProperties()?.GatewayAddresses)
+                .Select(g => g?.Address)
+                .Where(a => a != null)
+                // .Where(a => a.AddressFamily == AddressFamily.InterNetwork)
+                // .Where(a => Array.FindIndex(a.GetAddressBytes(), b => b != 0) >= 0)
+                .FirstOrDefault();
+        }
+
+        /**
+         * 执行CMD命令
+         * https://blog.csdn.net/Factor_/article/details/83690503
+         */
+        public void CMDOperator(string cmd)
+        {
+            Process myProcess = new Process();                 // 创建进程对象
+            myProcess.StartInfo.FileName = "cmd.exe";          // 设置打开cmd命令窗口
+            myProcess.StartInfo.UseShellExecute = false;       // 不使用操作系统shell启动进程的值
+            myProcess.StartInfo.RedirectStandardInput = true;  // 设置可以从标准输入流读取值
+            myProcess.StartInfo.RedirectStandardOutput = true; // 设置可以向标准输出流写入值
+            myProcess.StartInfo.RedirectStandardError = true;  // 设置可以显示输入输出流中出现的错误
+            myProcess.StartInfo.CreateNoWindow = true;         // 设置在新窗口中启动进程
+            myProcess.Start();                                 // 启动进程
+            myProcess.StandardInput.WriteLine(cmd);            // 传入要执行的命令
+        }
+
+        private void EDIT_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                // 1. 添加静态路由到原网关
+                CMDOperator("route add 192.168.0.0 mask 255.255.254.0 " + address.ToString());
+                // 2. 原地址转到新的网关
+                byte[] addressBytes = this.address.GetAddressBytes();
+                if (addressBytes[3] <= 253)
+                {
+                    addressBytes[3]++;
+                }
+                IPAddress newAddress = new IPAddress(addressBytes);
+                CMDOperator("route change 0.0.0.0 mask 0.0.0.0 " + newAddress.ToString());
+                this.EDIT.Enabled = false;
+                MessageBox.Show("设置成功");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("发生错误，请检查静态路由配置是否正确. " + exception.Message);
+            }
+        }
+
+        private void Yunzhi_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.yunzhi.LinkVisited = true;
+            // 跳转到首页
+            Process.Start("http://www.mengyunzhi.com/");
+        }
+    }
+}
